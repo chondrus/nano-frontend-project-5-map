8cd536f198302413f563e6945f92ddd9431b4a62
kels <chondrus@users.noreply.github.com>
2016-03-20 20:00:48 -0400

    not at all what I want but it works: earthquakes of the past 2.5 weeks

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 /*
  * knockout app management
  */
+var interestingLocations = [
+    {
+        name: '0th Location',
+        clickCount: 0,
+    },
+    {
+        name: '1st Location',
+        clickCount: 0,
+    }
+];
+
+var Location = function(data) {
+    this.name = ko.observable(data.name);
+    this.clickCount = ko.observable(data.clickCount);
+}
+
+
 var ViewModel = function() {
     var self = this;
 
@@ -11,18 +28,76 @@ var ViewModel = function() {
         self.counter(self.counter() + 1);
     };
 
-    // self.gmap = new initMap();
+    
 };
 
 ko.applyBindings(new ViewModel());
 
 /*
  * google maps funtionality
+ *
  */
+
+var map;
+
 function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
-        center: {lat: 44.540, lng: -78.546},
-        zoom: 8
+    map = new google.maps.Map(mapDiv, {
+        center: {lat: 42.359, lng: -71.062},
+        // zoom: 15
+        zoom: 2,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     });
+
+    // Create a <script> tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+
+    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
+
+function eqfeed_callback(results) {
+  map.data.addGeoJson(results);
+}
+
+// Call the initialize function after the page has finished loading
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
