9dbb097a1759eb83ece04cc6ac2c046d4993376a
kels <chondrus@users.noreply.github.com>
2016-04-09 14:47:51 -0400

    js for sidebar

---
+++ js/lib/offcanvas.js
@@ -0,0 +1,8 @@
+/*
+ * attribution: http://getbootstrap.com/examples/offcanvas/#
+ */
+$(document).ready(function () {
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
